d1565664d9fb0cea9a7103a237dcc2fac16dbc8c
Tucker Leach <leachtucker@gmail.com>
2020-09-01 14:27:20 -0700

    Try again for stretch tonight

--- stretch-assignment/index.js
+++
@@ -1,8 +0,0 @@
-// Blocks
-let redBlock = document.querySelector('.block--red');
-let blueBlock = document.querySelector('.block--blue');
-let greenBlock = document.querySelector('.block--green');
-let pinkBlock = document.querySelector('.block--pink');
-let grayBlock = document.querySelector('.block--gray');
-
-debugger;
