5f1a0c80c44518f384e7935681c9eabc582021a9
Tucker Leach <leachtucker@gmail.com>
2020-09-01 14:16:13 -0700

    Beginning work on stretch

---
+++ stretch-assignment/index.js
@@ -0,0 +1,8 @@
+// Blocks
+let redBlock = document.querySelector('.block--red');
+let blueBlock = document.querySelector('.block--blue');
+let greenBlock = document.querySelector('.block--green');
+let pinkBlock = document.querySelector('.block--pink');
+let grayBlock = document.querySelector('.block--gray');
+
+debugger;
